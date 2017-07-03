e0b3d813a3497734340303528ef3a9f7a00d9470
George Wilson <georgejameswilson@googlemail.com>
2017-07-03 17:35:35 +0100

    Code style part 2

--- libraries/src/CMS/Router/ApiRouter.php
+++ libraries/src/CMS/Router/ApiRouter.php
@@ -38,13 +38,52 @@ class ApiRouter extends Router
 	public function createCRUDRoutes($baseName, $controller, $defaults = array())
 	{
 		$routes = array(
-			array('method' => 'GET', 'pattern' => $baseName, 'controller' => $controller . '@display', 'defaults' => $defaults),
-			array('method' => 'GET', 'pattern' => $baseName . '/new', 'controller' => $controller . '@add', 'defaults' => $defaults),
-			array('method' => 'GET', 'pattern' => $baseName . '/:id', 'controller' => $controller . '@display', 'rules' => array('id' => '(\d+)'), 'defaults' => $defaults),
-			array('method' => 'GET', 'pattern' => $baseName . '/:id/edit', 'controller' => $controller . '@edit', 'rules' => array('id' => '(\d+)'), 'defaults' => $defaults),
-			array('method' => 'POST', 'pattern' => $baseName, 'controller' => $controller . '@add', 'defaults' => $defaults),
-			array('method' => 'PUT', 'pattern' => $baseName . '/:id', 'controller' => $controller . '@edit', 'rules' => array('id' => '(\d+)'), 'defaults' => $defaults),
-			array('method' => 'DELETE', 'pattern' => $baseName . '/:id', 'controller' => $controller . '@delete', 'rules' => array('id' => '(\d+)'), 'defaults' => $defaults),
+			array(
+				'method' => 'GET',
+				'pattern' => $baseName,
+				'controller' => $controller . '@display',
+				'defaults' => $defaults
+			),
+			array(
+				'method' => 'GET',
+				'pattern' => $baseName . '/new',
+				'controller' => $controller . '@add',
+				'defaults' => $defaults
+			),
+			array(
+				'method' => 'GET',
+				'pattern' => $baseName . '/:id',
+				'controller' => $controller . '@display',
+				'rules' => array('id' => '(\d+)'),
+				'defaults' => $defaults
+			),
+			array(
+				'method' => 'GET',
+				'pattern' => $baseName . '/:id/edit',
+				'controller' => $controller . '@edit',
+				'rules' => array('id' => '(\d+)'),
+				'defaults' => $defaults
+			),
+			array(
+				'method' => 'POST',
+				'pattern' => $baseName,
+				'controller' => $controller . '@add',
+				'defaults' => $defaults
+			),
+			array(
+				'method' => 'PUT',
+				'pattern' => $baseName . '/:id',
+				'controller' => $controller . '@edit',
+				'rules' => array('id' => '(\d+)'),
+				'defaults' => $defaults
+			),
+			array(
+				'method' => 'DELETE',
+				'pattern' => $baseName . '/:id',
+				'controller' => $controller . '@delete',
+				'rules' => array('id' => '(\d+)'),
+				'defaults' => $defaults
+			),
 		);
 		$this->addRoutes($routes);
 	}
