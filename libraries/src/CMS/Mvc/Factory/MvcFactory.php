d55b347b30285dd06efeb92f90c032cd1d599ed2
wilsonge <georgejameswilson@googlemail.com>
2017-08-11 15:42:55 +0100

    Hack back in support

--- libraries/src/CMS/Mvc/Factory/MvcFactory.php
+++ libraries/src/CMS/Mvc/Factory/MvcFactory.php
@@ -62,6 +62,11 @@ class MvcFactory implements MvcFactoryInterface
 	 */
 	public function createModel($name, $prefix = '', array $config = array())
 	{
+		if (empty($prefix) && $this->application->isClient('api'))
+		{
+			$prefix = 'Administrator';
+		}
+
 		// Clean the parameters
 		$name   = preg_replace('/[^A-Z0-9_]/i', '', $name);
 		$prefix = preg_replace('/[^A-Z0-9_]/i', '', $prefix);
