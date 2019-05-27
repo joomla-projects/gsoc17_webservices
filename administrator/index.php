8acaba1556e30befd075468af6360ac273272a38
zero-24 <zero-24@users.noreply.github.com>
2019-05-27 22:02:23 +0200

    Update index.php

--- administrator/index.php
+++ administrator/index.php
@@ -10,7 +10,7 @@
  * NOTE: This file should remain compatible with PHP 5.2 to allow us to run our PHP minimum check and show a friendly error message
  */
  
-/**
+/** 
  * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
  */
 define('JOOMLA_MINIMUM_PHP', '7.0');
